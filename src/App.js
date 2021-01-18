81d08ef921683ceaf7bc716fa9aafa3353b3ed88
Jose Saad <josemiguelsaad13@gmail.com>
2021-01-18 15:38:36 -0400

    login en version movil

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { TextField, Button } from '@material-ui/core';
+import { createMuiTheme, withStyles, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+
+const CssTextField = withStyles({
+  root: {
+    width: '60%',
+    '& label.Mui-focused': {
+      color: '#475694',
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: '#475694',
+    },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: '#b39490',
+      },
+      '&:hover fieldset': {
+        borderColor: '#b39490',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: '#475694',
+      },
+    },
+  },
+})(TextField);
+
+const ColorButton = withStyles((theme) => ({
+  root: {
+    color: theme.palette.getContrastText("#475694"),
+    backgroundColor: "#475694",
+    width: '60%',
+    height: 45,
+    '&:hover': {
+      backgroundColor: "#475694",
+    },
+  },
+}))(Button);
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title" />
+      <form className="login">
+      <CssTextField
+        className={classes.margin}
+        label="Correo UCAB"
+        variant="outlined"
+        id="custom-css-outlined-input-email"
+      />
+        <div className="appart">
+          <CssTextField
+            className={classes.margin}
+            label="Contraseña"
+            type="password"
+            variant="outlined"
+            id="custom-css-outlined-input-pass"
+          />
+        </div>
+      </form>
+      <ColorButton variant="contained" color="primary" className={classes.margin}>
+        Ingresar
+      </ColorButton>
     </div>
   );
 }
