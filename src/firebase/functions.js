e6770a86a6505b20641d11d442330694e123ab03
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2021-01-24 00:05:46 -0400

    Registrar usuario
    Función con Firebase

--- src/firebase/functions.js
+++ src/firebase/functions.js
@@ -1,5 +1,28 @@
-import { auth } from "./config";
+import { auth, db } from "./config";
 
 export const userLogin = (email, password) => {
   return auth.signInWithEmailAndPassword(email, password);
+};
+
+export const userSignUp = async (user) => {
+  const response = await auth.createUserWithEmailAndPassword(
+    user.email,
+    user.password
+  );
+
+  const uid = response.user.uid;
+
+  const data = {
+    // TODO: Considerar luego si poner el uid aquí o no
+    name: user.name,
+    // TODO: Consirar alternativas para estos nombres
+    idDocument: user.idDocument,
+    degree:  user.degree,
+    // TODO: Lo mismo del uid con el email
+    email: user.email,
+    phone: user.phone,
+    isTutor: user.isTutor,
+  };
+
+  return db.collection("students").doc(uid).set(data);
 };
