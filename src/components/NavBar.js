ec6c5d79c49384df50c7b23071120f781a4c3e40
Jose Saad <josemiguelsaad13@gmail.com>
2021-01-29 19:08:02 -0400

    drawer con algunos iconos

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import SvgIcon from '@material-ui/core/SvgIcon';
-import SearchIcon from '@material-ui/icons/Search';
-import InputBase from '@material-ui/core/InputBase';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import LogOutIcon from '@material-ui/icons/ExitToApp'
 import { ReactComponent as BarLogo } from "../assets/bar-logo.svg";
-import { ReactComponent as BarDarkMode } from "../assets/bar-darkmode.svg";
+import BarDarkMode from '@material-ui/icons/Brightness4';
 import DrawerIcon from '@material-ui/icons/Menu';
 import Drawer from '@material-ui/core/Drawer';
 import "./NavBar.css";
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import HomeIcon from '@material-ui/icons/Home'
+import SearchIcon from '@material-ui/icons/Search'
+import Divider from '@material-ui/core/Divider';
+import EditT from '@material-ui/icons/Edit'
 
 function NavBar() {
     const [open, setOpen] = useState(false)
@@ -55,7 +57,27 @@ function NavBar() {
         onClose={() => setOpen(false)}
       >
         <div className="drawerContainer">
-            <h1>Hoola</h1>
+            <ListItem button>
+            <ListItemIcon>
+                <HomeIcon/>
+            </ListItemIcon>
+            <ListItemText primary="Inicio" />
+            </ListItem>
+            <ListItem button>
+            <ListItemIcon>
+                <SearchIcon />
+            </ListItemIcon>
+            <ListItemText primary="Buscar Tutorías" />
+            </ListItem>
+            <div className="tutorOptions">
+                <Divider style={{marginTop: '5pt', marginBottom: '5pt'}}/>
+                <ListItem button>
+                <ListItemIcon>
+                    <EditT />
+                </ListItemIcon>
+                <ListItemText primary="Editar Tutorías" />
+                </ListItem>
+            </div>
         </div>
       </Drawer>
     </>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,7 +16,7 @@ const Home = () => {
       <NavBar />
     </div>
     <div className="HomeBackground">
-        
+      
     </div>
     </>
   );
