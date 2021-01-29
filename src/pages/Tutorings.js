593b2c58163b1206ac6cee4781347572b0db65f5
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2021-01-28 23:13:47 -0400

    Seleccionar carrera para tutorías
    El Select puesto

--- src/pages/Tutorings.js
+++ src/pages/Tutorings.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useUser } from "../contexts/UserContext";
 import { getTutoringsByDegree } from "../firebase/functions";
+import { TextField, MenuItem } from "@material-ui/core";
+import { degrees } from "../degrees";
 
 const Tutorings = () => {
   const user = useUser();
@@ -16,9 +18,27 @@ const Tutorings = () => {
     });
   }, [degree]);
 
+  const handleChangeDegree = (e) => {
+    setDegree(e.target.value);
+  };
+
   return (
     <div>
       <h1>Lista de Tutorías</h1>
+      <TextField
+        select
+        label="Seleccionar Carrera"
+        variant="outlined"
+        onChange={(e) => handleChangeDegree(e)}
+        value={degree}
+      >
+        {degrees.map((option) => (
+          <MenuItem key={option.id} value={option.id}>
+            {option.name}
+          </MenuItem>
+        ))}
+      </TextField>
+      <h2>{degree}</h2>
     </div>
   );
 };
